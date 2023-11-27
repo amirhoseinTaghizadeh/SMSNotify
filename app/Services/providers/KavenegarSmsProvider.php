f77ff316340631ad0d199b175343abbb81c698f1
AmirHoseinTaghizadeh <taghizadehamirhosein9@gmail.com>
2023-11-27 17:55:52 +0330

    add simple-factory method

--- app/Services/providers/KavenegarSmsProvider.php
+++ app/Services/providers/KavenegarSmsProvider.php
@@ -8,6 +8,27 @@ class KavenegarSmsProvider implements SmsService
 {
     public function sendSms($phoneNumber, $message)
     {
-        // TODO: Implement sendSms() method.
+        try {
+            $sender = "10004346";
+            $receptor = [$phoneNumber];
+            $result = Kavenegar::Send($sender, $receptor, $message);
+
+            foreach ($result as $r) {
+                return "messageid = $r->messageid";
+                return "message = $r->message";
+                return "status = $r->status";
+                return "statustext = $r->statustext";
+                return "sender = $r->sender";
+                return "receptor = $r->receptor";
+                return "date = $r->date";
+                return "cost = $r->cost";
+            }
+        } catch (\Kavenegar\Exceptions\ApiException $e) {
+            echo $e->errorMessage();
+        } catch (\Kavenegar\Exceptions\HttpException $e) {
+            echo $e->errorMessage();
+        } catch (\Exception $ex) {
+            echo $ex->getMessage();
+        }
     }
 }
